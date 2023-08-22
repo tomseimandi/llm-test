eb49348e72c3f0182b1236309ad4d2e4f22e3e77
tseimandi <tom.seimandi@insee.fr>
2023-08-22 08:02:32 +0000

    Add gitignore and chromadb creation function

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,12 @@
 # Constants
+import torch
 
 
 EMBEDDINGS_MODEL = 'sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2'
 # EMBEDDINGS_MODEL = 'sentence-transformers/all-MiniLM-L6-v2'
+
+# Device
+if torch.cuda.is_available():
+    DEVICE = 'cuda'
+else:
+    DEVICE = 'cpu'
--- src/db_build.py
+++ src/db_build.py
@@ -1,34 +1,66 @@
 # File: db_build.py
-import torch
-
-from langchain.vectorstores import FAISS
+import argparse
+from typing import Literal
+from langchain.vectorstores import FAISS, Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.embeddings import HuggingFaceEmbeddings
-from constants import EMBEDDINGS_MODEL
+from constants import EMBEDDINGS_MODEL, DEVICE
 
 
-# Device
-if torch.cuda.is_available():
-    device = 'cuda'
-else:
-    device = 'cpu'
+def create_faiss_database(documents, embeddings):
+    """Build and persist FAISS vector store.
 
-# Load PDF file from data path
-loader = DirectoryLoader('data/',
-                         glob="*.pdf",
-                         loader_cls=PyPDFLoader)
-documents = loader.load()
+    Args:
+        database_name: Text documents.
+        embeddings: Embeddings.
+    """
+    vectorstore = FAISS.from_documents(documents, embeddings)
+    vectorstore.save_local('vectorstore/db_faiss')
 
-# Split text from PDF into chunks
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500,
-                                               chunk_overlap=50)
-texts = text_splitter.split_documents(documents)
 
-# Load embeddings model
-embeddings = HuggingFaceEmbeddings(model_name=EMBEDDINGS_MODEL,
-                                   model_kwargs={'device': device})
+def create_chroma_database(documents, embeddings):
+    """Build and persist Chroma vector store.
 
-# Build and persist FAISS vector store
-vectorstore = FAISS.from_documents(texts, embeddings)
-vectorstore.save_local('vectorstore/db_faiss')
+    Args:
+        database_name: Text documents.
+        embeddings: Embeddings.
+    """
+    vectorstore = Chroma.from_documents(documents, embeddings)
+    vectorstore.save_local('vectorstore/db_chroma')
+
+
+def main(database_name: Literal["faiss", "chroma"]):
+    """Create FAISS of Chroma vector store for embeddings
+
+    Args:
+        database_name (Literal["faiss", "chroma"]): Database type.
+    """
+    # Load PDF file from data path
+    loader = DirectoryLoader('data/',
+                             glob="*.pdf",
+                             loader_cls=PyPDFLoader)
+    raw_documents = loader.load()
+
+    # Split text from PDF into chunks
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500,
+                                                   chunk_overlap=50)
+    documents = text_splitter.split_documents(raw_documents)
+
+    # Load embeddings model
+    embeddings = HuggingFaceEmbeddings(model_name=EMBEDDINGS_MODEL,
+                                       model_kwargs={'device': DEVICE})
+    if database_name == "faiss":
+        create_faiss_database(documents, embeddings)
+    elif database_name == "chroma":
+        create_chroma_database(documents, embeddings)
+    else:
+        raise ValueError("Database name must be 'faiss' or 'chroma'")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('database_name', type=str)
+    args = parser.parse_args()
+
+    main(database_name=args.database_name)
